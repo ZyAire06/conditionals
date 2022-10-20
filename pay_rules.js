83cb680f02a9483a545ca1942b09063c44c74ecc
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-20 19:58:26 -0400

    playing with switches

--- pay_rules.js
+++ pay_rules.js
@@ -1,16 +1,28 @@
 let payRate=15.00;
 
 let hoursWorked = 45.00;
+let overtime= payRate*1.5;
 
-let check= payRate*hoursWorked;
-console.log(`Total of emepolyees grosspay will be $${check} without overtime.`);
+let grossPay= overtime*hoursWorked;
+let check=payRate*hoursWorked;
+// console.log(`Total of emepolyees grosspay will be $${check} without overtime.`);
 
-if(hoursWorked<40){ 
-    overtime= payRate*1.5;
+// if(hoursWorked<40){ 
+//     overtime= payRate*1.5;
+// }
+// console.log(`With overtime the emepolyee hourly wage will be $${overtime}.`);
+
+// if( overtime==22.50){
+//     grossPay= overtime*hoursWorked
+// }
+// console.log(`Total of emepolyees grosspay will be $${grossPay} with overtime.`);
+
+switch(hoursWorked){
+    case(40):
+         check;
+        break;
+    case(45):
+        grossPay;
+        break;
 }
-console.log(`With overtime the emepolyee hourly wage will be $${overtime}.`);
-
-if( overtime==22.50){
-    grossPay= overtime*hoursWorked
-}
-console.log(`Total of emepolyees grosspay will be $${grossPay} with overtime.`);
+console.log(hoursWorked);
